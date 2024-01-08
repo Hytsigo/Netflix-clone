7b455d2d3b5b7111bd9b38e3b69304640fddcccc
hytsigo <hytsigo1@gmail.com>
2024-01-07 21:25:15 -0500

    Primera version solo vista

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3dRUmY-Q1VAiBHyNYMxIhwFWDP8geJGo",
+    authDomain: "netflix-clone-1c819.firebaseapp.com",
+    projectId: "netflix-clone-1c819",
+    storageBucket: "netflix-clone-1c819.appspot.com",
+    messagingSenderId: "279558000228",
+    appId: "1:279558000228:web:0e18d58599d72d50c19971",
+};
